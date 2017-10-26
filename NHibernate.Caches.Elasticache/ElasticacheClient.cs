7b100bb8c0f2046b05edba87c7e111dd60e2f098
Dan Sargeant <dan@keywordintent.com>
2017-10-26 22:25:01 +1000

    Update async methods to better handle cancellation token.

--- NHibernate.Caches.Elasticache/ElasticacheClient.cs
+++ NHibernate.Caches.Elasticache/ElasticacheClient.cs
@@ -161,36 +161,59 @@ namespace NHibernate.Caches.Elasticache
 
         public Task<object> GetAsync(object key, CancellationToken cancellationToken)
         {
-            var result = Get(key);
-            return Task.FromResult(result);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<object>(cancellationToken);
+            }
+            return Task.FromResult(Get(key));
         }
 
         public Task PutAsync(object key, object value, CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
             Put(key, value);
             return Task.CompletedTask;
         }
 
         public Task RemoveAsync(object key, CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
             Remove(key);
             return Task.CompletedTask;
         }
 
         public Task ClearAsync(CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
             Clear();
             return Task.CompletedTask;
         }
 
         public Task LockAsync(object key, CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
             Lock(key);
             return Task.CompletedTask;
         }
 
         public Task UnlockAsync(object key, CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
             Unlock(key);
             return Task.CompletedTask;
         }
